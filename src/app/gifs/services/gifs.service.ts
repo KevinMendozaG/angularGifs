9568d48ca77098d4cd772a078c79edbcefbdbfda
Kevin Mendoza <kevin.ymg@hotmail.com>
2022-01-10 18:19:21 -0500

    end

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey: string = 'DuqexyJxoo86MFblfQpx9cjTd294mfn3';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
   
   public resultados: Gif[] =[];
@@ -16,7 +17,16 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+
+    this.resultados = JSON.parse( localStorage.getItem('resultados')! ) || [];
+    // if ( localStorage.getItem('historial')) {
+    //   this._historial = JSON.parse( localStorage.getItem('historial')! );  
+    // }
+
+  }
 
 
   buscarGifs( query: string ) {
@@ -28,12 +38,20 @@ export class GifsService {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
   
+      localStorage.setItem('historial', JSON.stringify( this._historial ) );
+      
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=DuqexyJxoo86MFblfQpx9cjTd294mfn3&q=${ query } z&limit=10`)
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
         .subscribe( ( resp ) => {
           console.log(resp.data);
           this.resultados = resp.data;
+          localStorage.setItem('resultados', JSON.stringify( this.resultados ) );
         })
 
     // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=DuqexyJxoo86MFblfQpx9cjTd294mfn3&q=dragon ball z&limit=10')
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -16,5 +16,8 @@ export class SidebarComponent{
   
   constructor ( private gifsService: GifsService ) {}
 
+  buscar ( termino: string) {
+    this.gifsService.buscarGifs(termino);
+  }
 
 }
